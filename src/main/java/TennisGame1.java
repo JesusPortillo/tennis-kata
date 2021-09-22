c7fba85dbb6c52cb99644a56068f96cd02b2b375
JesusPortillo <jesusdavidpv12@gmail.com>
2021-09-22 18:37:44 -0500

    Clase TennisGame1 refactorizada

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,10 +1,10 @@
-
 public class TennisGame1 implements TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+
+    private int scorePlayer1 = 0;
+    private int scorePlayer2 = 0;
     private String player1Name;
     private String player2Name;
+
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -12,64 +12,90 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName.equals(player1Name))
+            scorePlayer1 += 1;
+        if (playerName.equals(player2Name))
+            scorePlayer2 += 1;
     }
+
+    public String selectScoresWhenAreEqual(int scorePlayer){
+        String score = "";
+        switch (scorePlayer) {
+            case 0:
+                score = "Love-All";
+                return score;
+            case 1:
+                score = "Fifteen-All";
+                return score;
+            case 2:
+                score = "Thirty-All";
+                return score;
+            default:
+                score = "Deuce";
+                return score;
+        }
+    }
+
+    public String selectScoreByAdvantage(int minusResult){
+        String score = "";
+        switch (minusResult){
+            case 1:
+                score = "Advantage player1";
+                return score;
+            case -1:
+                score = "Advantage player2";
+                return score;
+            default:
+                return selectPlayerWinner(minusResult);
+        }
+    }
+
+    public String selectPlayerWinner(int minusResult) {
+        if (isMinusToTwo(minusResult)) return "Win for "+this.player1Name;
+        return "Win for "+this.player2Name;
+    }
+
+    private boolean isMinusToTwo(int minusResult) {
+        return minusResult>=2;
+    }
+
+    public int calculateDifference(int scorePlayer1, int scorePlayer2){
+        return scorePlayer1 - scorePlayer2;
+    }
+
+
+    public String selectScoreForEachPlayer(int scorePlayer){
+        String score = "";
+        switch(scorePlayer)
+        {
+            case 0:
+                score+="Love";
+                return score;
+            case 1:
+                score+="Fifteen";
+                return score;
+            case 2:
+                score+="Thirty";
+                return score;
+            default:
+                score+="Forty";
+                return score;
+        }
+    }
+    public boolean isThereAnyAdvantage (){return scorePlayer1 >=4 || scorePlayer2 >=4;}
+    public boolean areEqual(){return scorePlayer1 == scorePlayer2;}
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        if (areEqual()) {
+            score = selectScoresWhenAreEqual(scorePlayer1);
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+        else if (isThereAnyAdvantage()) {
+            int minusResult = calculateDifference(scorePlayer1, scorePlayer2);
+            score = selectScoreByAdvantage(minusResult);
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        else {
+            score = selectScoreForEachPlayer(scorePlayer1) + "-" + selectScoreForEachPlayer(scorePlayer2);
         }
         return score;
     }
