09628c0247e79469c97ecfc579fa58139385ebee
JesusPortillo <jesusdavidpv12@gmail.com>
2021-09-22 21:04:49 -0500

    la clase TennisGame3 fué factorizada

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -1,35 +1,63 @@
 
 public class TennisGame3 implements TennisGame {
-    
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
+
+    private int pointsForPlayer1;
+    private int pointsForPlayer2;
+    private String player1Name;
+    private String player2Name;
+    private String[] points = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
 
     public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+        this.player1Name = p1N;
+        this.player2Name = p2N;
+    }
+
+    public String getPoint(int pointsForPlayer1){
+        return points[pointsForPlayer1];
+    }
+    public boolean canBeAssignTwoScores(){
+        return pointsForPlayer1 < 4 && pointsForPlayer2 < 4 && NoMoreThanSixPoints();}
+
+    private boolean NoMoreThanSixPoints() {
+        return (pointsForPlayer1 + pointsForPlayer2 != 6);
+    }
+
+    public boolean areEqualsInThree(){
+        return pointsForPlayer1 == pointsForPlayer2;
+    }
+    public String carryTheSamePoints(String score){
+        return (pointsForPlayer1 == pointsForPlayer2) ? score + "-All" : score + "-" + points[pointsForPlayer2];
+    }
+    public String nameOfTheCurrentWinner(){
+        return pointsForPlayer1 > pointsForPlayer2 ? player1Name : player2Name;
+    }
+
+    public String advantageOrWin(String score){
+        return ( difference() == 1) ? "Advantage " + score : "Win for " + score;
+    }
+
+    private int difference() {
+        return (pointsForPlayer1 - pointsForPlayer2)*(pointsForPlayer1 - pointsForPlayer2);
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+        String score;
+        if (canBeAssignTwoScores()) {
+            score=getPoint(pointsForPlayer1);
+            return carryTheSamePoints(score);
         } else {
-            if (p1 == p2)
+            if (areEqualsInThree())
                 return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            score = nameOfTheCurrentWinner();
+            return advantageOrWin(score);
         }
     }
     
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            this.p1 += 1;
+            this.pointsForPlayer1 += 1;
         else
-            this.p2 += 1;
+            this.pointsForPlayer2 += 1;
         
     }
 
